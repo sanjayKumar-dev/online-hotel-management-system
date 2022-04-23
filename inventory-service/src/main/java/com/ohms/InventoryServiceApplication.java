1de10dc6b9b79fb48fb77444e5cf6062000a6f3b
sanjayKumar-dev <sanjay.11801603@gmail.com>
2022-04-23 20:05:37 +0530

    Added Opendoc in inventory-service

--- inventory-service/src/main/java/com/ohms/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ohms/InventoryServiceApplication.java
@@ -3,7 +3,11 @@ package com.ohms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title="Product API",version="1.0",description="Product microservice"))
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
