e4a3abd1aacd6f32592a697870e9704a061c8bba
sanjayKumar-dev <sanjay.11801603@gmail.com>
2022-04-24 10:36:31 +0530

    added the API Gateway

---
+++ API-Gateway/src/main/java/com/ohms/config/ApiGatewayConfiguration.java
@@ -0,0 +1,35 @@
+package com.ohms.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+	
+	public RouteLocator routeLocatorInvenory(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p->p
+						.path("/guest/get")
+						.uri("http://localhost:8081")
+						)
+				.route(p->p
+						.path("/guest/add")
+						.uri("http://localhost:8081")
+						)
+				.route(p->p
+						.path("/guest/get/{id}")
+						.uri("http://localhost:8081")
+						)
+				.route(p->p
+						.path("/guest/update/{id}")
+						.uri("http://localhost:8081")
+						)
+				.route(p->p
+						.path("/guest/delete/{id}")
+						.uri("http://localhost:8081")
+						)
+				.build();		
+	}
+
+}
